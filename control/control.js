178e4ca75b12b500f41edd8f161b822dc7eb3b0c
capogreco <thomas.capogreco@gmail.com>
2022-12-30 14:23:10 +1030

    changing websocket server address

--- control/control.js
+++ control/control.js
@@ -2,8 +2,7 @@
 
 let id = null
 
-// const ws_address = `wss://capogreco-ds-asweat.deno.dev/`
-const ws_address = `ws://localhost/`
+const ws_address = `wss://capogreco-omni.deno.dev`
 
 const socket = new WebSocket (ws_address)
 
